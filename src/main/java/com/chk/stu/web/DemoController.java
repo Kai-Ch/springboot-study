a30330dc09acd8cb9a324446e4e952c5dc85d86a
kaichenr <kaichenr@isoftstone.com>
2019-12-16 22:19:41 +0800

    modify swagger

--- src/main/java/com/chk/stu/web/DemoController.java
+++ src/main/java/com/chk/stu/web/DemoController.java
@@ -2,6 +2,7 @@ package com.chk.stu.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @description:
  * @Date:2019/11/26 22:52
  */
-@RestController("/demo")
+@RequestMapping(value = "/demo")
+@RestController
 public class DemoController {
 
     @GetMapping("/hello/{msg}")
--- src/main/java/com/chk/stu/web/EmpController.java
+++ src/main/java/com/chk/stu/web/EmpController.java
@@ -4,7 +4,8 @@ import com.chk.stu.domain.Emp;
 import com.chk.stu.service.IEmpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,15 +15,20 @@ import java.util.List;
  * @description:
  * @Date:2019/11/28 21:37
  */
-@RestController("/emp")
+@RequestMapping(value = "/emp")
+@RestController
 public class EmpController {
 
     @Autowired
     private IEmpService iEmpService;
 
     @GetMapping("/all")
-    @ResponseBody
     public List<Emp> findAll() {
         return iEmpService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Emp findById(@PathVariable Integer id) {
+        return iEmpService.findById(id);
     }
 }
